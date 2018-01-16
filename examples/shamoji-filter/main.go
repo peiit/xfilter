e5d6d73d439298da236e1a2010e02be4f46c0850
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-01-16 15:13:26 +0100

    add shamoji-filter

---
+++ examples/shamoji-filter/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/sniperkit/xfilter/backend/shamoji"
+	"github.com/sniperkit/xfilter/backend/shamoji/filter"
+	"github.com/sniperkit/xfilter/backend/shamoji/tokenizer"
+	"golang.org/x/text/unicode/norm"
+)
+
+var (
+	o sync.Once
+	s *shamoji.Serve
+)
+
+func main() {
+	yes, word := Contains("No regret of one piece in my life")
+	fmt.Printf("Result: %v, Word: %s", yes, word)
+}
+
+func Contains(sentence string) (bool, string) {
+	o.Do(func() {
+		s = &shamoji.Serve{
+			Tokenizer: tokenizer.NewKagomeSimpleTokenizer(norm.NFKC),
+			Filer:     filter.NewCuckooFilter("In the long run", "Repentance"),
+		}
+	})
+	return s.Do(sentence)
+}
